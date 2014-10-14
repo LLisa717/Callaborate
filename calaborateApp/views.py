c88aeda3966894964093b20ae4dae897efb4036d
lisa qianyun li <lisali.com@gmail.com>
2014-10-14 01:31:20 -0700

    added render_to_respond

--- calaborateApp/views.py
+++ calaborateApp/views.py
@@ -1,6 +1,7 @@
 from django.template import RequestContext, Template, Context
 from django.views.decorators.csrf import csrf_exempt
 
+from django.shortcuts import render_to_response
 # Create your views here.
 @csrf_exempt 
 def index(request):
