5c615f825253ddc7fd05cd308d2c4284e7893b44
zacatac <field.zackery@gmail.com>
2014-10-17 14:27:21 -0700

    collecstatic settings should now work

--- calaborate/settings.py
+++ calaborate/settings.py
@@ -121,33 +121,35 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-
+print BASE_DIR
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-from django.core.exceptions import ImproperlyConfigured
+print STATICFILES_DIRS
+# from django.core.exceptions import ImproperlyConfigured
 
-def get_env_variable(var_name):
-    """ Get the environment variable or return exception """
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        error_msg = "Set the %s environment variable" % var_name
-        raise ImproperlyConfigured(error_msg)
+# def get_env_variable(var_name):
+#     """ Get the environment variable or return exception """
+#     try:
+#         return os.environ[var_name]
+#     except KeyError:
+#         error_msg = "Set the %s environment variable" % var_name
+#         raise ImproperlyConfigured(error_msg)
 
-group = get_env_variable('DJANGO_SETTINGS_GROUP')
+# group = get_env_variable('DJANGO_SETTINGS_GROUP')
 
-if group == 'local':
-    from settings.local import *
-elif group == 'staging':
-    from settings.staging import *
-elif group == 'production':
-    from settings.production import *
-else:
-    raise ImproperlyConfigured('DJANGO_SETTINGS_GROUP env var must be set to either "local", "staging", or "production"')
+# if group == 'local':
+#     from settings_groups.local import *
+# elif group == 'staging':
+#     from settings_groups.staging import *
+# elif group == 'production':
+#     from settings_groups.production import *
+# else:
+#     raise ImproperlyConfigured('DJANGO_SETTINGS_GROUP env var must be set to either "local", "staging", or "production"')
 
